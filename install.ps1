8d8c2c8bdf16ad63aad290eb9bf2eca54bffb7f7
rmens <r.mens@me.com>
2024-06-09 12:00:35 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,16 +1,34 @@
 #===============================================================
-# Set variables
+# Windows 11 Baseline for Streekomroep ZuidWest
 #===============================================================
 
-# Prompt for system type and purpose
+# Function to prompt for user input
+function Get-UserInput {
+    param (
+        [string]$PromptMessage
+    )
+    return Read-Host -Prompt $PromptMessage
+}
+
+# Prompt for system purpose
+$purpose = Get-UserInput -PromptMessage "Enter the system purpose:"
+[System.Environment]::SetEnvironmentVariable("_deploy_purpose", $purpose, [System.EnvironmentVariableTarget]::Machine)
+
+# Prompt for system ownership
+$ownership = Get-UserInput -PromptMessage "Enter the system ownership:"
+[System.Environment]::SetEnvironmentVariable("_deploy_ownership", $ownership, [System.EnvironmentVariableTarget]::Machine)
 
 # Prompt for user password
-$deploy_user_password = Read-Host -Prompt "Geef het wachtwoord voor de gebruiker in"
+$deploy_user_password = Get-UserInput -PromptMessage "Enter the user password:"
 [System.Environment]::SetEnvironmentVariable("_deploy_user_password", $deploy_user_password, [System.EnvironmentVariableTarget]::Machine)
 
 # Prompt for computer name
-$deploy_computer_name = Read-Host -Prompt "Vul computernaam in"
+$deploy_computer_name = Get-UserInput -PromptMessage "Enter the computer name:"
 [System.Environment]::SetEnvironmentVariable("_deploy_computer_name", $deploy_computer_name, [System.EnvironmentVariableTarget]::Machine)
+
+# Prompt for workgroup
+$workgroup = Get-UserInput -PromptMessage "Enter the workgroup name:"
+[System.Environment]::SetEnvironmentVariable("_deploy_workgroup", $workgroup, [System.EnvironmentVariableTarget]::Machine)
 
 #===============================================================
 # Cleanup directory
@@ -19,4 +37,18 @@ $deploy_computer_name = Read-Host -Prompt "Vul computernaam in"
 $deployDirectory = "C:\Windows\deploy"
 
 # Recreate the directory forcefully
+if (Test-Path $deployDirectory) {
+    Remove-Item -Path $deployDirectory -Recurse -Force
+}
 New-Item -Path $deployDirectory -ItemType Directory -Force
+
+#===============================================================
+# Call sub-scripts
+#===============================================================
+
+# TODO: Implement a loop that calls all the script
+$installScriptPath = "C:\windows\deploy\apps.ps1"
+$arguments = "-purpose `"$purpose`" -ownership `"$ownership`""
+Start-Process -FilePath "powershell.exe" -ArgumentList "-File `"$installScriptPath`" $arguments" -Wait -NoNewWindow
+
+Write-Output "Script execution completed."
